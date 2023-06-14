5281d6a6a8ab2b7809cf31bee2bf6bcec8a6bb53
Abukarimov <abdukarimovmuslimbek2004@gmail.com>
2023-06-14 15:37:01 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Login/>
+  </div>
+</template>
+
+<script setup>
+import Login from './views/Login.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>home  wqdqdq</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
